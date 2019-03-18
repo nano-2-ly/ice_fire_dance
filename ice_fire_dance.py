1eff4ba28514020f328d584c18ddc60cba384064
nano-2-ly <dong991207@unist.ac.kr>
2019-03-18 23:14:30 +0900

    update for dinosaurse

--- ice_fire_dance.py
+++ ice_fire_dance.py
@@ -1,21 +1,30 @@
 import sounddevice as sd
 import numpy as np
 from pynput.keyboard import Key, Controller
+import time
+
 keyboard = Controller()
-fs = 4400                           
+fs = 44100                               
 duration = 5 # seconds
-while(1):
-    myrecording = sd.rec(10 , samplerate=fs, channels=1, dtype='float64')
+while(1): 
+    myrecording = sd.rec(4410 , samplerate=fs, channels=1, dtype='float64')
     # I would like to read myrecording into a stream at this point. 
     #print("Recording Audio")
     sd.wait()
     #print("Audio recording complete , Play Audio")
     #print(myrecording)
+    keyboard.press(Key.down)
+    
     for i in myrecording:
-        if i>0.1:
-            print("!")
+        if i>0.1 :      
+            #print("!")
+            keyboard.release(Key.down)
+            #time.sleep(1)
             keyboard.press(Key.space)
+            
+
             keyboard.release(Key.space)
-            break
+            time.sleep(0.1) 
+            break               
 plt.plot(myrecording)
-plt.show()
+plt.show() 
